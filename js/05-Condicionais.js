86130082c97984c6cb11004b98508d131d3f8ec4
ricardoshiga <roppongi_forever@hotmail.com>
2023-04-06 16:28:12 -0300

    Condicional encadeada

--- js/05-Condicionais.js
+++ js/05-Condicionais.js
@@ -15,9 +15,9 @@ Operadores relacionais:
 != (DIFERENTE) */
 
 // Exemplos
-const aluno = "Chaves";
+const aluno = "Ricardo";
 let nota1 = 10;
-let nota2 = 5;
+let nota2 = 9;
 let faltas = 7;
 let media = (nota1 + nota2) / 2;
 
@@ -32,10 +32,39 @@ const limiteDeFaltas = 10;
 
 /* ALGORITMO Se a média obtida pelo aluno for maior ou igual à média da escola (e se suas faltas forem abaixo do limite), ele estará aprovado. Caso contrário, ele estará reprovado.*/
 
-if (media >= mediaMinima && faltas <limiteDeFaltas ) {
+/* Condicional Composta */
+if (media >= mediaMinima && faltas < limiteDeFaltas) {
     console.log("aprovado");
 } else {
     console.log("reprovado");
 }
+
+/* Condicinal Encadeada */
+
+/* Algoritmo para análise de desempenho
+Média menor que 3? PÉSSIMO
+Média menor que 5? RUIM
+Média menor que 7? REGULAR
+Média menor que 9? BOM
+Nenhuma das anteriores? ÓTIMO */
+
+
+let desempenho;
+
+if (media < 3) {
+    desempenho = "PÉSSIMO";
+} else if (media < 5) {
+    desempenho = "RUIM";
+} else if (media < 7) {
+    desempenho = "REGULAR";
+} else if (media < 9) {
+    desempenho = "BOM";
+} else {
+    desempenho = "ÓTIMO";
+}
+
+console.log(desempenho);
+
+
 
 
