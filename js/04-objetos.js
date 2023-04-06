27b8d4442c351b37e0ba5c66ea5aed22e59e83a0
ricardoshiga <roppongi_forever@hotmail.com>
2023-04-06 14:51:28 -0300

    sei la

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -60,7 +60,31 @@ let livros = [
  console.log(livros[1].titulo);
  console.log(livros[0].autor);
 
+console.log("..................................")
 
+/* Criar objeto chamado PEDIDO, contendo as seguintes informações:
 
+- Código (invente uma combinação de letras e números)
+- Lista de 3 produtos (basta o nome de cada um)
+- Data do pedido (data de hoje)
+- Preço
+- Prazo de entrega em dias
 
+2) Mostre no console uma frase contendo algumas das infomrações do pedido. 
+   Exemplo: "Referente ao pedido 123xyz, os produtos foram comprados em 06/04/2023 custando 4500; O prazo de entrega é de 7 dias úteis." */
 
+   let pedido = {
+            codigo: "ri1974",
+            produtos: [ "feijão", "arroz", "alho"],
+            data: "06/05/2023",
+            preco: 740,
+            prazo: "7"
+        
+   
+}   
+   let configuracoes = {
+    style: "currency",
+    currency: "BRL"
+   }
+
+   console.log(`Referente ao pedido ${pedido.codigo} os produtos foram comprados em ${pedido.data}, custando ${pedido.preco.toLocaleString("pt-br", configuracoes)}, com prazo de entrega de ${pedido.prazo} dias úteis.`);
