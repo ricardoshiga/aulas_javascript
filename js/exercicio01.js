f9e8b19c4796a221b4da748f2a8744b6d58a7086
ricardoshiga <roppongi_forever@hotmail.com>
2023-04-11 16:16:02 -0300

    exercicio1 com refatoração

--- js/exercicio01.js
+++ js/exercicio01.js
@@ -5,17 +5,31 @@ let nome = prompt("Qual seu nome?");
 let idade = parseIntprompt("Qual sua idade?")
 
 // LÓGICA DAS FAIXAS DE IDADE
+// if (idade >= 18 && idade < 60) {
+//     console.log(`${nome}, você é uma pessoa adulta`);
+//     alert(`${nome}, você é uma pessoa adulta`)
+// } else if (idade >= 60) {
+//     console.log(`${nome}, você é uma pessoa idosa`);
+//     alert(`${nome}, você é uma pessoa idosa`)
+// } else {
+//     console.log(`${nome}, você é uma pessoa menor de idade`);
+//     alert(`${nome}, você é uma pessoa menor de idade`)
+// }
+
+// COM REFATORAÇÃO
+let mensagem; 
 if (idade >= 18 && idade < 60) {
-    console.log(`${nome}, você é uma pessoa adulta`);
-    alert(`${nome}, você é uma pessoa adulta`)
+    mensagem = "adulta";
+    
 } else if (idade >= 60) {
-    console.log(`${nome}, você é uma pessoa idosa`);
-    alert(`${nome}, você é uma pessoa idosa`)
+    mensagem = "idosa";
+    
 } else {
-    console.log(`${nome}, você é uma pessoa menor de idade`);
-    alert(`${nome}, você é uma pessoa menor de idade`)
+    mensagem = "menor de idade";
+    
 }
 
+alert(`${nome}, você é uma pessoa ${mensagem}`);
 
 
 
