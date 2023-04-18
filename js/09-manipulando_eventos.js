804e3da009ca9400b95862dc71e982e3aa24ba4a
ricardoshiga <roppongi_forever@hotmail.com>
2023-04-18 16:50:24 -0300

    termino do exercicio 3

--- js/09-manipulando_eventos.js
+++ js/09-manipulando_eventos.js
@@ -1,4 +1,4 @@
-/* Exemplo 1*/
+// Exemplo 1
 const exemplo1 = document.querySelector("span");
 
 /* Ouvinte/Monitor de Evento 
@@ -18,13 +18,20 @@ exemplo1.addEventListener ("mouseout", function(){
     exemplo1.textContent = "Exemplo 01";
     exemplo1.style.all = "unset";
 
+    if(pagina.classList.contains("modo-noturno")) {
+       exemplo1.style.color = "white";
+}   else {
+    exemplo1.style.color = "black";
+}
+
+
     exemplo1.style.fontFamily = "Times New Roman";
     exemplo1.style.textTransform = "normal";
     exemplo1.style.color = "black"
     
 } );
 
-/* Exemplo 2: Modo noturno */
+// Exemplo 2: Modo noturno
 
 // Etapa 1: selecionar o objeto/elemento que será modificado
 const pagina = document.querySelector("body");
@@ -50,3 +57,66 @@ if(botao.textContent == "Ativar")
 }
 
 });
+
+// Exemplo 3: simulação de cadastro 
+
+/*  Captura de dados de formulário, 
+    cálculos de valores, 
+    verificação condicional, 
+    ceiação de HTML via JS, 
+    adição de conteúdo/resultados na página (DOM) 
+*/
+
+/* Etapa 1:
+Acessar e selecionar os elementos que serão usados*/
+const formulario = document.querySelector("form");
+const campoNome = document.querySelector("#nome");
+const campoNota1 = document.querySelector("#nota1");
+const campoNota2 = document.querySelector("#nota2");
+const divResultados = document.querySelector("#resultados")
+/*  Etapa 2:
+Monitorar o evento de acionamento de formulário,
+ou seja, detectar quando o usuário clica no botão de formulário ou pressiona ENTER. */
+formulario.addEventListener("submit", function(event){
+    /* Usamos o comando abaixo par aanular o comportamento padrão do navegador de tentar dazer um redirecionamento de página para acionar o formulário. Isso é importante para as ações programadas na função  possam ser initerpretadas. */
+    event.preventDefault()
+    
+/* Etapa 3:
+ Capturar os valores digitados DENTRO de cada campo*/
+ let nome = campoNome.value;   
+ let nota1 = campoNota1.value;   
+ let nota2 = campoNota2.value;   
+
+ /* Exercícios */
+
+ const media = (nota1 + nota2) / 2;
+
+ const mediaMinima = 7;
+ let resultado; 
+if (media >= mediaMinima) {
+    resultado = "Aprovado";
+} else {
+    resultado = "Reprovado";
+}
+
+console.log((`O Aluno ${nome} de notas ${nota1} e ${nota2} e foi ${resultado}`));
+
+/* Programação para colocar os resultados no HTML */
+// 1) Criar o elemento/tag que será colocado no HTML
+let paragrafo = document.createElement("p")
+
+// 2) Montar o conteúdo da nova tag
+paragrafo.innerHTML = `${nome} - ${media} - ${resultado}`;
+
+// 3) Adicionar ao HTML de destino (injetar no DOM)
+divResultados.appendChild(paragrafo);
+
+// Limpar campos
+formulario.reset();
+
+// Devolver o foco (cursor) para o campo nome
+campoNome.focus ();
+
+});
+
+
